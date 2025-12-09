1f84c4ff9cdfb2294d12e57fe0ee6ac920e7f6ca
AsokTamang <ashoktmg2058@gmail.com>
2025-12-09 11:30:37 -0500

    handling the error situation

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,43 +1,45 @@
-import path from 'node:path'
-import fs from 'node:fs/promises'
-import { sendResponse } from './sendResponse.js'
+import path from "node:path";
+import fs from "node:fs/promises";
+import { sendResponse } from "./sendResponse.js";
+import { getContentType } from "./getContentType.js";
 
+export async function serveStatic(req, res, baseDir) {
+  /*
+Challenge: 
+  1. Write code below to serve files from our public directory.
+     
+     Don’t worry about handling errors for now.
+     hint.md for help!
+*/
+  const publ = path.join(baseDir, "public"); //this points to the public dir
+  const filePath = path.join(publ, req.url === "/" ? "index.html" : req.url); //this points to index.html if the req url is / otherwise just to the page where the url of the req matches
+  const extensions = path.extname(filePath); //this gives us the extensions of the filepath that we are displaying
 
-
-export async function serveStatic(req,res,baseDir) {
-  
-  const filePath = path.join(baseDir, 'public', 'index.html')  //this gives us the location of a file that we want to use
   try {
-    const content = await fs.readFile(filePath)  //this code is for reading the file 
-    
-    sendResponse(res,200,'text/html',content)  //here we are passing the header type as an object as name and value as key-value pair
-    
-  } catch (error) {
-    console.log(error);
-    
+    const content = await fs.readFile(filePath);
+    sendResponse(res, 200, getContentType(extensions), content); //here for the content-type we are using getcontenttype function by passing the extensions as parameter
+  } catch (err) {
+    /*
+Challenge:
+
+ If the error code is “ENOENT”, serve the 404.html page.  
+ If there’s another error, serve a 500 with this string: 
+ `<html><h1>Server Error: ${err.code}</h1></html>`. 
+
+The Content-Type for the 500 can be ‘text/html’.
+*/
+
+    if (err.code === "ENOENT") {
+      const filepath = path.join(publ, "404.html");
+      const content = await fs.readFile(filepath); //so we are reading the 404.html file
+      sendResponse(res, 404, "text/html", content);
+    } else {
+      sendResponse(
+        res,
+        500,
+        "text/html",
+        `<html><h1>Server Error: ${err.code}</h1></html>`
+      );
+    }
   }
-  
-
-
-/*
-Challenge 1: 
-
-- Store index.html as a buffer in a const ‘content’. 
-- As this is an async process, do this inside a try/catch block.
-- For now, just log the error in the catch block.
-- You will need to change something to do with the function declaration. What is it?
-
-*/
-
-/*
-Challenge 3:
-
-- Import sendResponse() and use it to serve index.html. 
-  Pass in all of the information sendResponse() is expecting.
-  serveStatic() will need another param. What is it?
-
-  Make any changes necessary in server.js and delete any unneeded code.
-
-*/
-
 }
