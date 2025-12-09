91cde1bc178d0bc27060e56cb713798658d4cd49
AsokTamang <ashoktmg2058@gmail.com>
2025-12-09 00:02:18 -0500

    dirname of the current working module

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import http from "http";
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
+  //we also have a res.writeHead method (statuscode , {'Content-Type':'application/json'})  where we can directly send the status code along with the content type and access-control-allow-methods with the method values ,and this is in a object
+  //inorder to get the dir name of the current module that we are working on
+  //we must use import.meta
+  const __dirname = import.meta.dirname;
+  
+  
   res.end(
     `<html>
       <h1>The server is working</h1>
