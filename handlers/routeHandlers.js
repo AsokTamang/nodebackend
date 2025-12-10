fbfedb6fc155cc94a2d04946f512eb6aa805693d
AsokTamang <ashoktmg2058@gmail.com>
2025-12-10 17:58:29 -0500

    sanitizing the user input to prevent from cross-site scripting

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -2,16 +2,28 @@ import { getData } from "../utils/getData";
 import { sendResponse } from "../utils/sendResponse";
 import { parseJSONBody } from "../utils/parseJSONBody";
 import { addNewSighting } from "../utils/addNewSighting";
+
+import sanitize from "sanitize-html";
 export async function handleGet(res) {
   const content = await getData();
   sendResponse(res, 200, "application/json", JSON.stringify(content)); //as the getData is returning the parsed json data , we must stringify it first
 }
 
+function sanitizer(obj) {  //clean-up function
+  for (const k of Object.keys(obj)) {
+    //iterating over the keys of json object
+    obj[k] = sanitize(obj[k], { allowedTags: ["b"], allowedAttributes: [] }); //no attributes are allowed and only the b tag which is bold tag is been allowed in our sanitize method
+  }
+  return obj;
+}
+
 export async function handlePost(req, res) {
   try {
     const parsedBody = await parseJSONBody(req);
-    await addNewSighting(parsedBody)  //here we are passng the parsed newsighting in our addnewsighting function to add in the existing database
-    sendResponse(res, 201, "application/json", JSON.stringify(parsedBody));  
+    const newdata = sanitizer(parsedBody);
+    await addNewSighting(newdata); //here we are passng the parsed newsighting in our addnewsighting function to add in the existing database
+
+    sendResponse(res, 201, "application/json", JSON.stringify(newdata));
   } catch (err) {
     sendResponse(res, 400, "application/json", JSON.stringify({ error: err }));
   }
