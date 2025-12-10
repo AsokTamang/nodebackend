e4700c7cf58bda3887317c98a1298726dfcf233b
AsokTamang <ashoktmg2058@gmail.com>
2025-12-10 18:24:20 -0500

    event emitter instance done

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -2,7 +2,7 @@ import { getData } from "../utils/getData";
 import { sendResponse } from "../utils/sendResponse";
 import { parseJSONBody } from "../utils/parseJSONBody";
 import { addNewSighting } from "../utils/addNewSighting";
-
+import { sightingEvents } from "../events/sightingEvents";
 import sanitize from "sanitize-html";
 export async function handleGet(res) {
   const content = await getData();
@@ -22,7 +22,7 @@ export async function handlePost(req, res) {
     const parsedBody = await parseJSONBody(req);
     const newdata = sanitizer(parsedBody);
     await addNewSighting(newdata); //here we are passng the parsed newsighting in our addnewsighting function to add in the existing database
-
+    sightingEvents.emit('sighting-added',newdata);    //as soon as the new data of haunted place is added, we are emitting this data using eventemitter instance we created 
     sendResponse(res, 201, "application/json", JSON.stringify(newdata));
   } catch (err) {
     sendResponse(res, 400, "application/json", JSON.stringify({ error: err }));
