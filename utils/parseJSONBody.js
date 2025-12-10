9d1df32ea95f0abf37009860afb0299f5f7642bd
AsokTamang <ashoktmg2058@gmail.com>
2025-12-10 17:03:57 -0500

    parsed on the route handler

--- utils/parseJSONBody.js
+++ utils/parseJSONBody.js
@@ -5,7 +5,7 @@ export async function parseJSONBody(req) {
       //in the req of the node js , there are load of chunks of datas that are incoming asynchronously , thats why we are using the for loop and await on these chunks of datas ,added in the body
       body += chunk;
     }
-    return JSON.parse(body);
+    return JSON.parse(body);  //JSON.parse is used for parsing the datas
   } catch (err) {
     throw new Error(`Invalid JSON format: ${err}`);
   }
