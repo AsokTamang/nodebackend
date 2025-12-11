bf3ed7ebfb7908856a299e5e569b182e507ab04f
AsokTamang <ashoktmg2058@gmail.com>
2025-12-11 10:41:54 -0500

    db

--- events/sightingEvents.js
+++ events/sightingEvents.js
@@ -5,3 +5,6 @@ import { createAlert } from '../utils/createAlert.js'
 export const sightingEvents = new EventEmitter();
 sightingEvents.on('sighting-added',createAlert);     //the instance of eventemitter called sightingEvents listen on the event sighting-added with the function createAlert  
 
+
+
+//in the self-hosted database , the data can be more secured where as in the cloud-database , the data can be less secured as the security depends upon the third-party
